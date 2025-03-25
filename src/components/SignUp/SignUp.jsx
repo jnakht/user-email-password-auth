534a37b69fcf159c8aa268dbc9d4e00e8a2f1f56
jnakht <jisan.cse.cu@gmail.com>
2025-03-25 15:27:54 +0600

    email and password got from register

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,10 +1,16 @@
 
 
 const SignUp = () => {
+    const handleRegister = e => {
+        e.preventDefault();
+        const userEmail = e.target.email.value;
+        const userPassword = e.target.password.value;
+        console.log('submitted with email: ', userEmail + ' password: ', userPassword);
+    }
     return (
         <div className=" mt-[60px]">
             <h3 className="text-3xl mb-4">Please Register</h3>
-            <form className=" flex flex-col">
+            <form onSubmit={handleRegister} className=" flex flex-col">
                 <input className=" bg-slate-500 mb-4 py-2 px-4" type="email" placeholder="Email Address" name="email" id="" />
                 <input className="bg-slate-500 mb-4 py-2 px-4" type="password" placeholder="Password" name="password" id="" />
                 <input className="btn btn-primary" type="submit" value="Submit" />
